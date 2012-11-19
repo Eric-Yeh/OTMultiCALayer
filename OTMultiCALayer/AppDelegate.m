cae4648e28523914a6c9b5dddef6f2af6aeca4e9
Eric Yeh <lanter77@gmail.com>
2012-11-19 17:40:12 +0800

    Initial Commit

---
+++ OTMultiCALayer/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  OTMultiCALayer
+//
+//  Created by Hank0272 on 12/11/19.
+//  Copyright (c) 2012年 Eric Yeh. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ OTMultiCALayer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  OTMultiCALayer
+//
+//  Created by Hank0272 on 12/11/19.
+//  Copyright (c) 2012年 Eric Yeh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
